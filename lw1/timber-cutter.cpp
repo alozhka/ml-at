94c34c6210068366e3a917d492084d7f35451b73
Sergey <100339481+alozhka@users.noreply.github.com>
2025-02-16 17:56:35 +0300

    feat: правки 1 лабы

--- lw1/timber-cutter.cpp
+++ lw1/timber-cutter.cpp
@@ -3,23 +3,17 @@
 #include <ostream>
 #include <vector>
 
-class TimberCutter
-{
-  static void CutInternal(std::vector<int>& timbers, const int times, int cost, int& minCost)
-  {
-    if (times == 0)
-    {
-      if (cost < minCost)
-      {
+class TimberCutter {
+  static void CutInternal(std::vector<int> &timbers, const int times, int cost, int &minCost) {
+    if (times == 0) {
+      if (cost < minCost) {
         minCost = cost;
       }
       return;
     }
 
-    for (int i = 0; i < timbers.size(); i++)
-    {
-      for (int j = 1; j < timbers[i]; j++)
-      {
+    for (int i = 0; i < timbers.size(); i++) {
+      for (int j = 1; j < timbers[i]; j++) {
         int left = j;
         int right = timbers[i] - j;
 
@@ -32,11 +26,11 @@ class TimberCutter
       }
     }
   }
+
 public:
-  static int Cut(const int length, const int times)
-  {
+  static int Cut(const int length, const int times) {
     int cost = INT_MAX;
-    std::vector timbers = { length };
+    std::vector timbers = {length};
 
     CutInternal(timbers, times, 0, cost);
 
@@ -44,7 +38,6 @@ public:
   }
 };
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   std::cout << TimberCutter::Cut(std::atoi(argv[1]), std::atoi(argv[2])) << std::endl;
 }
