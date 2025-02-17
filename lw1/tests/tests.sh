67fdc87b2acd4081a531d69353397a17811af207
Sergey <100339481+alozhka@users.noreply.github.com>
2025-02-17 15:33:05 +0300

    feat: оптимизировал распил бруса 2 (лаба 1)

--- lw1/tests/tests.sh
+++ lw1/tests/tests.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-../cmake-build-debug/timber-cutter 10 4
-../cmake-build-debug/timber-cutter 100 3
+echo 7 и 6 = 20
+../cmake-build-release/timber-cutter 7 6
+echo 10 и 3 = 15
+../cmake-build-release/timber-cutter 10 3
+echo 9 и 7 = 27
+../cmake-build-release/timber-cutter 9 7
+echo 10 и 7 = 29
+../cmake-build-release/timber-cutter 10 7
+echo 13 и 10 = 44
+../cmake-build-release/timber-cutter 13 10
+echo 10 и 4 = 18
+../cmake-build-release/timber-cutter 10 4
+echo 100 и 3 = 105
+../cmake-build-release/timber-cutter 100 3
--- lw1/timber-cutter.cpp
+++ lw1/timber-cutter.cpp
@@ -1,4 +1,22 @@
+/*
+Ложкин Сергей, ПС-21
+Среда: CMake
+
+14.10. Распил бруса 2 (10)
+На пилораму привезли брус длиной L метров. Требуется сделать N распилов.
+Распилы делят брус на части, длина которых выражается натуральными числами. Стоимость одного распила равна длине распиливаемого бруса. Определить минимальную стоимость распила.
+Ввод. В первой строке содержатся через пробел натуральные числа L (2 ≤ L ≤ 106) и N (N < L) – длина бруса и число распилов.
+Вывод. В единственной строке вывести минимальную стоимость распилов.
+
+Примеры
+Ввод 1         Ввод 2
+100 3          10 4
+Вывод 1        Вывод 2
+105            18
+*/
+
 #include <climits>
+#include <cmath>
 #include <iostream>
 #include <ostream>
 #include <stack>
@@ -25,16 +43,21 @@ public:
       }
 
       for (int i = 0; i < timbers.size(); i++) {
-        for (int j = 1; j < timbers[i]; j++) {
+        for (int j = 1; j <= std::floor(timbers[i] / 2); j++) {
           int left = j;
           int right = timbers[i] - j;
+          int newCost = cost + timbers[i];
 
-          std::vector<int> newTimbers = {};
-          std::ranges::copy(timbers, std::back_inserter(newTimbers));
-          auto it = newTimbers.erase(newTimbers.begin() + i);
-          newTimbers.insert(it, {left, right});
+          if (newCost >= minCost) {
+            continue;
+          }
 
-          dataStack.emplace(newTimbers, times - 1, cost + timbers[i]);
+          std::vector newTimbers{timbers};
+          newTimbers.erase(newTimbers.begin() + i);
+          newTimbers.push_back(left);
+          newTimbers.push_back(right);
+
+          dataStack.emplace(newTimbers, times - 1, newCost);
         }
       }
     }
