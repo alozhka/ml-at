5f5ea3fe17768aa888729c4f79c1dbed9bac9691
Sergey Lozhkin <alozhka@yahoo.com>
2025-05-18 21:14:47 +0300

    feat: добавил оптимизацию в алгоритм

--- 6.1.lucky-ticket/main.cpp
+++ 6.1.lucky-ticket/main.cpp
@@ -1,10 +1,21 @@
 /*
 Ложкин Сергей, ПС-21
+Среда: CMake
 
-6.1. Счастливые билеты (8). Номера автобусных билетов состоят из 2N цифр, включая незначащие нули (1 ≤ N ≤ 500). Минимальный возможный номер состоит из 2N нулей, а максимальный – из 2N девяток. Студент Вася ездит в университет на автобусе и коллекционирует счастливые билеты, в которых сумма первых N цифр совпадает с суммой последних N цифр. Как и всякий настоящий коллекционер, Вася готов пойти на все для пополнения своей коллекции. Вместе с тем ему пока приходится учитывать свои финансовые возможности.
-Каждый раз после приобретения билета Вася определяет, какое минимальное количество билетов нужно купить еще, чтобы среди них обязательно оказался счастливый билет. Если у него оказывается достаточно денег, он тут же покупает необходимое число билетов. Иногда он не успевает закончить расчеты, проезжает свою остановку и опаздывает на занятия. Помогите Васе.
+6.1. Счастливые билеты (8). Номера автобусных билетов состоят из 2N цифр, включая незначащие нули (1 ≤ N ≤ 500).
+Минимальный возможный номер состоит из 2N нулей, а максимальный – из 2N девяток.
+Студент Вася ездит в университет на автобусе и коллекционирует счастливые билеты,
+в которых сумма первых N цифр совпадает с суммой последних N цифр. Как и всякий настоящий коллекционер,
+Вася готов пойти на все для пополнения своей коллекции. Вместе с тем ему пока приходится учитывать свои финансовые возможности.
+Каждый раз после приобретения билета Вася определяет, какое минимальное количество билетов нужно купить еще,
+чтобы среди них обязательно оказался счастливый билет. Если у него оказывается достаточно денег,
+он тут же покупает необходимое число билетов. Иногда он не успевает закончить расчеты,
+проезжает свою остановку и опаздывает на занятия. Помогите Васе.
+
 Ввод из файла INPUT.TXT. В первой строке находится число N. Во второй строке задан номер первого билета Васи из 2N цифр.
+
 Вывод в файл OUTPUT.TXT. В единственной строке вывести минимальное число следующих билетов, необходимое для гарантированного получения счастливого билета.
+
 Примеры
 Ввод 1    Ввод 2    Ввод 3
 2         2         2
@@ -13,65 +24,107 @@
 12        5         0
 */
 #include <fstream>
+#include <span>
 #include <string>
+#include <vector>
 
 using ull = unsigned long long;
 
-void ReadTicket(std::string_view filename, int& N, std::string& ticket)
+void readInput(std::string_view filename, int& N, std::string& ticket)
 {
-	std::ifstream in(filename.data());
-	in >> N >> ticket;
+	std::ifstream fin(filename.data());
+	fin >> N >> ticket;
+	fin.close();
 }
 
-void PrintAnswer(std::string_view filename, ull answer)
+void writeOutput(std::string_view filename, ull steps)
 {
-	std::ofstream out(filename.data());
-	out << answer << std::endl;
+	std::ofstream fout(filename.data());
+	fout << steps << "\n";
+	fout.close();
 }
 
-bool IsLucky(const std::string& ticket, int N)
+// Увеличиваем номер билета на 1
+void incrementTicket(std::vector<short>& digits)
 {
-	int left_sum = 0, right_sum = 0;
-	for (int i = 0; i < N; i++)
+	int n = digits.size();
+	int carry = 1;
+	for (int i = n - 1; i >= 0 && carry; --i)
 	{
-		left_sum += ticket[i] - '0';
+		int sum = digits[i] + carry;
+		digits[i] = sum % 10;
+		carry = sum / 10;
 	}
-	for (int i = N; i < 2 * N; i++)
-	{
-		right_sum += ticket[i] - '0';
-	}
-	return left_sum == right_sum;
 }
 
-std::string numberToTicket(ull number, int totalLength)
+ull digitsToNumber(const std::vector<short>& digits)
 {
-	std::string result(totalLength, '0');
-	for (int i = totalLength - 1; i >= 0; i--)
+	ull res = 0;
+	for (const int d : digits)
 	{
-		result[i] = number % 10 + '0';
-		number /= 10;
+		res *= 10;
+		res += d;
 	}
-	return result;
+	return res;
 }
 
-ull FindMinLengthToTicket(int N, const std::string& startTicket)
+void fillVector(std::vector<short>& v, short n)
 {
-	ull current = 0;
-	for (char c : startTicket)
+	for (short& i : v)
 	{
-		current = current * 10 + (c - '0');
+		i = n;
+	}
+}
+
+ull CalculateSum(const std::vector<short>& v)
+{
+	ull res = 0;
+	std::ranges::for_each(v, [&res](short digit) { res += digit; });
+	return res;
+}
+
+ull solve(int N, const std::string& ticketStr)
+{
+	std::vector<short> digits(2 * N);
+	std::vector<short> s1(N);
+	std::vector<short> s2(N);
+	for (int i = 0; i < 2 * N; ++i)
+	{
+		digits[i] = static_cast<short>(ticketStr[i] - '0');
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		s1[i] = digits[i];
+	}
+	for (int i = 0; i <= N; ++i)
+	{
+		s2[i] = digits[i + N];
 	}
 
-	int totalLength = 2 * N;
 	ull steps = 0;
+	ull sum1 = CalculateSum(s1);
+	ull sum2 = CalculateSum(s2);
 
-	for (; true; current++, steps++)
+	if (sum1 == sum2)
 	{
-		std::string ticket = numberToTicket(current, totalLength);
-		if (IsLucky(ticket, N))
+		return 0;
+	}
+
+	while (sum1 != sum2)
+	{
+		if (sum1 < s2.front())
 		{
-			break;
+			incrementTicket(s1);
+			steps += std::pow(10, N) - digitsToNumber(s2);
+			fillVector(s2, 0);
+			continue;
 		}
+
+		incrementTicket(s2);
+		++steps;
+
+		sum1 = CalculateSum(s1);
+		sum2 = CalculateSum(s2);
 	}
 
 	return steps;
@@ -81,8 +134,8 @@ int main(const int _, const char* argv[])
 {
 	int N;
 	std::string ticket;
-	ReadTicket(argv[1], N, ticket);
-	const ull answer = FindMinLengthToTicket(N, ticket);
-	PrintAnswer(argv[2], answer);
+	readInput(argv[1], N, ticket);
+	ull answer = solve(N, ticket);
+	writeOutput(argv[2], answer);
 	return 0;
 }
